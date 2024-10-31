b79b8fd2b1e576d4427e4c00614cd80e66f25ff1
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 21:37:53 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'MaDHouSe79' 
 description 'mh-boobytrap'
-version '1.0'
+version '1.0.0'
 
 shared_scripts {
 	'@qb-core/shared/locale.lua',
@@ -18,4 +18,5 @@ client_scripts {
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
 	'server/main.lua',
+	'server/update.lua',
 }
